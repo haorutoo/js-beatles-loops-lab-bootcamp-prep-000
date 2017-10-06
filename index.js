e6b565d9ee1ae21dc0c1f3dee07a8c26f5db0a94
haorutoo <haorutoo@gmail.com>
2017-10-06 15:15:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function theBeatlesPlay(musicians,instruments){
-  //var obj = { musicians, instruments}
+  var obj = { musicians, instruments}
   //var newObj = Object.assign({}, obj)
   var newObj //= Object.assign({})
 
-  for (i = 0, i < musicians.length, i++){
+  for (i = 0, i < obj.length, i++){
     //newObj = Object.assign({}, '${musicians[i]} plays ${instruments[i]}')
     var musician = musicians[i]
-    var instrument = instruments[i] 
+    var instrument = instruments[i]
     newObj.push('${musician} plays ${instrument}')
   }
 
